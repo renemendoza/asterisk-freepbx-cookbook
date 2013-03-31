4ff01c57a6251ac5da01fb4b0f1e40eba3bd6543
Rene Mendoza <latiendamac@gmail.com>
2013-03-30 23:08:49 -0600

    please build asterisk-mysql

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['asterisk']['use_digium_repo']  = false
-default['asterisk']['packages']         = ['asterisk']
+default['asterisk']['packages']         = ['asterisk', 'asterisk-mysql']
 
 #Setup the Manager.conf file, refer to: http://www.voip-info.org/tiki-index.php?page=Asterisk%20config%20manager.conf
 #[general]
